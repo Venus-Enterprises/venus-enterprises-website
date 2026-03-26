2c1d7371b6d8babd25df19596f761a39a9be0f5e
Dinesh0515 <ddelecs01@gmail.com>
2026-03-26 16:23:31 +0530

    Fix Next.js AppProps typing for production build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import FloatingWhatsApp from "@/modules/FloatingWhatsApp/FloatingWhatsApp";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
